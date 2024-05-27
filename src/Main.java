8ce676ee56abacae00f5833425e3ffa10c33e08c
jochongs <joch2712@naver.com>
2024-05-27 23:38:44 +0900

    feat: Create ServerSocket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(80);
+
+            Socket socket = serverSocket.accept();
+
+            System.out.println("socket = " + socket);
+        } catch (IOException exception) {
+            System.out.println("exception = " + exception);
+        }
     }
 }
