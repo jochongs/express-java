be13ff0fda9d9d594fb35cfd548a3a36a22a8154
jochongs <joch2712@naver.com>
2024-05-29 00:29:46 +0900

    feat: Create RequestHeader

--- src/request/RequestHeader.java
+++ src/request/RequestHeader.java
@@ -1,2 +1,35 @@
-package request;public class RequestHeader {
+package request;
+
+import jdk.jfr.ContentType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestHeader {
+    String authorization;
+    String cookie;
+    String host;
+    String origin;
+    String contentType;
+    String contentLength;
+
+    private RequestHeader (
+            String authorization,
+            String cookie,
+            String host,
+            String origin,
+            String contentType,
+            String contentLength
+    ) {
+        this.authorization = authorization;
+        this.cookie = cookie;
+        this.host = host;
+        this.origin = origin;
+        this.contentType = contentType;
+        this.contentLength = contentLength;
+    }
+
+    public RequestHeader(RawRequest rawRequest) {
+        Map<String, String> headerMap = new HashMap<>();
+    }
 }
