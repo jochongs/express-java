ac2c6abd3e7298ca733c525fec51898cbd730de2
jochongs <joch2712@naver.com>
2024-05-29 17:49:51 +0900

    feat: Create HttpMethod class

---
+++ src/HttpMethod.java
@@ -0,0 +1,7 @@
+public class HttpMethod {
+    public final static String GET = "GET";
+    public final static String POST = "POST";
+    public final static String PUT = "PUT";
+    public final static String DELETE = "DELETE";
+    public final static String PATCH = "PATCH";
+}
