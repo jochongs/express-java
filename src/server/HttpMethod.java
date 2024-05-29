82e38881b854bfdfecef6cd5929dd6ae78dd106e
jochongs <joch2712@naver.com>
2024-05-29 19:26:34 +0900

    feat: Add static getHttpMethod method

--- src/server/HttpMethod.java
+++ src/server/HttpMethod.java
@@ -1,5 +1,25 @@
 package server;
 
 public enum HttpMethod {
-    GET, POST, PUT, DELETE, PATCH, OPTION
+    GET, POST, PUT, DELETE, PATCH, OPTION;
+
+    public static HttpMethod getMethod(String method) {
+        if (method.equals("GET")) {
+            return HttpMethod.GET;
+        }
+        if (method.equals("POST")) {
+            return HttpMethod.POST;
+        }
+        if (method.equals("PUT")) {
+            return HttpMethod.PUT;
+        }
+        if (method.equals("DELETE")) {
+            return HttpMethod.DELETE;
+        }
+        if (method.equals("OPTION")) {
+            return HttpMethod.OPTION;
+        }
+
+        throw new IllegalArgumentException("No matching method");
+    }
 }
