caba71b332a9b383029dc43ab0fdea5abc3c70bc
jochongs <joch2712@naver.com>
2024-05-30 14:38:41 +0900

    feat: Create HttpException child Exceptions

--- src/exception/BadRequestException.java
+++ src/exception/BadRequestException.java
@@ -1,2 +1,7 @@
-package exception;public class BadRequestException {
+package exception;
+
+public class BadRequestException extends HttpException {
+    public BadRequestException(String message) {
+        super(401, message);
+    }
 }
---
+++ src/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class UnauthorizedException extends HttpException {
+    public UnauthorizedException(String message) {
+        super(401, message);
+    }
+}
